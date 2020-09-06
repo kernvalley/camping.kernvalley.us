9ee44e8225dd1c60e4336dbe4f53f76ffb4c8a04
Chris Zuber <shgysk8zer0@gmail.com>
2020-09-06 16:29:49 -0700

    Performance improvements
    - Implement preloading
    - Provide background images in different sizes
    - Default all request referrer policy to `"no-referrer"`
    - Update various components/`_include/`s
    - Dependency updates
    - Misc. other updates

--- js/index.js
+++ js/index.js
@@ -12,15 +12,56 @@ import 'https://cdn.kernvalley.us/components/leaflet/marker.js';
 import 'https://cdn.kernvalley.us/components/pwa/install.js';
 import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 import { HTMLNotificationElement } from 'https://cdn.kernvalley.us/components/notification/html-notification.js';
+import { importGa } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
+import { loadScript } from 'https://cdn.kernvalley.us/js/std-js/loader.js';
 import { registerMapSearch } from './functions.js';
 import { hashChange, stateHandler } from './handlers.js';
-import { site } from './consts.js';
+import { site, GA } from './consts.js';
 
 document.documentElement.classList.replace('no-js', 'js');
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
 
-ready().then(async () => {
+if (typeof GA === 'string' && GA.length !== 0) {
+	importGa(GA).then(async () => {
+		/* global ga */
+		ga('create', ga, 'auto');
+		ga('set', 'transport', 'beacon');
+		ga('send', 'pageview');
+
+
+		function outbound() {
+			ga('send', {
+				hitType: 'event',
+				eventCategory: 'outbound',
+				eventAction: 'click',
+				eventLabel: this.href,
+				transport: 'beacon',
+			});
+		}
+
+		function madeCall() {
+			ga('send', {
+				hitType: 'event',
+				eventCategory: 'call',
+				eventLabel: 'Called',
+				transport: 'beacon',
+			});
+		}
+
+		await ready();
+
+		$('a[rel~="external"]').click(outbound, { passive: true, capture: true });
+		$('a[href^="tel:"]').click(madeCall, { passive: true, capture: true });
+
+	});
+}
+
+
+Promise.allSettled([
+	ready(),
+	loadScript('https://cdn.polyfill.io/v3/polyfill.min.js'),
+]).then(async () => {
 	if (location.pathname === '/') {
 		registerMapSearch();
 		addEventListener('popstate', stateHandler);
